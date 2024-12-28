ddfe85d01662d87c3a54faa74fb29ed10e52e92a
karthi-l <karthi9442685754@gmail.com>
2024-12-28 12:47:01 +0530

    corrected the navbar

--- index.php
+++ index.php
@@ -13,39 +13,38 @@
             <nav class="navbar navbar-dark fixed-top navbar-expand-lg bg-dark mx-2 mx-md-3 mx-lg-4 mx-xl-5 rounded p-1 shadow-sm">
                 <div class="container-fluid">
                     <a href="index.php" class="navbar-brand">
-                        <img src="img/st-logo-3.png" alt="" class="img-fluid rounded d-inline-block border border-white" width="120px">
+                        <img src="img/st-logo-3.png" alt="" id="nav-logo" class="img-fluid rounded d-inline-block border border-white" width="120px">
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav ms-auto">
-                            <li class="nav-item  mx-1 border border-white rounded "><a class="nav-link active text-light hov-nav nav-align" href="index.php">Home</a></li>
-                            <li class="nav-item  mx-1 border border-white rounded "><a href="about.php" class="nav-link active text-light hov-nav nav-align">About-Us</a></li>
-                            <li class="nav-item  mx-1 border border-white rounded "><a href="products.php" class="nav-link active text-light hov-nav nav-align">Products</a></li>
-                            <li class="nav-item  mx-1 border border-white rounded "><a href="catologue.php" class="nav-link active text-light hov-nav nav-align">Catologue</a></li>
-                            <li class="nav-item  mx-1 ">
+                            <li class="nav-item  mx-1 border border-white rounded my-1 my-lg-0"><a class="nav-link active text-light hov-nav nav-align" href="index.php">Home</a></li>
+                            <li class="nav-item  mx-1 border border-white rounded my-1 my-lg-0"><a href="about.php" class="nav-link active text-light hov-nav nav-align">About-Us</a></li>
+                            <li class="nav-item  mx-1 border border-white rounded my-1 my-lg-0"><a href="products.php" class="nav-link active text-light hov-nav nav-align">Products</a></li>
+                            <li class="nav-item  mx-1 border border-white rounded my-1 my-lg-0"><a href="catologue.php" class="nav-link active text-light hov-nav nav-align">Catologue</a></li>
+                            <li class="nav-item  mx-1 my-1 my-lg-0">
                                 <form class="d-flex ">
                                     <input class="form-control me-2 nav-align" type="search" placeholder="Search" aria-label="Search">
                                     <button class="btn btn-outline-success border border-white text-white nav-align" type="submit">Search</button>
                                   </form>
                             </li>
                             <li class="nav-item mx-1 border border-white rounded d-none d-lg-block"><a href="account.php" class="nav-link active hov-nav nav-align"><img src="img/account-logo.png" alt="Account" class="img-fluid" width=20px></a></li>
-                            <li class="nav-item mx-1 d-block d-lg-none">    
-                                <div class="row g-0">
-                                    <div class="col-6 mx-1 border border-white rounded">
-                                        <a href="login.php" class="nav-link active text-light hov-nav nav-align">Login</a>
+                            <li class="nav-item mx-1 d-block d-lg-none my-1 my-lg-0">    
+                                <div class="row">
+                                    <div class="col">
+                                        <a href="login.php" class="nav-link active text-light hov-nav nav-align border border-white rounded">Login</a>
                                     </div>
-                                    <div class="col-6 mx-1 border border-white rounded">
-                                        <a href="register.php" class="nav-link active text-light hov-nav nav-align">Register</a>
+                                    <div class="col">
+                                        <a href="register.php" class="nav-link active text-light hov-nav nav-align border border-white rounded">Register</a>
                                     </div>
                                 </div>
                             </li>
                         </ul>
                     </div>
                 </div>
-            </nav>
-           
+            </nav>    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
